37b856f75afad1ffd81b0e7e1c6b750cee824f7a
XkArras <141564729+XkArras@users.noreply.github.com>
2023-09-27 22:17:03 +0100

    Delete public directory

--- public/service-worker.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", async () => {
-    await self.registration.unregister();
-    for (var e of await self.clients.matchAll({ type: "window" })) e.navigate(e.url)
-});
