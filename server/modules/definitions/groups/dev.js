630aeb4ecb44af1dcdd70a073bb5bc78fb8678f0
Frostbyte178 <frostbyte178@gmail.com>
2023-12-24 23:06:16 -0800

    handful of fixes

--- server/modules/definitions/groups/dev.js
+++ server/modules/definitions/groups/dev.js
@@ -43,7 +43,13 @@ exports.developer = {
         FOV: 2,
     },
     RESET_CHILDREN: true,
+    ACCEPTS_SCORE: true,
+    CAN_BE_ON_LEADERBOARD: true,
+    DRAW_HEALTH: true,
+    ARENA_CLOSER: false,
     INVISIBLE: [0, 0],
+    ALPHA: [0, 1],
+    HITS_OWN_TYPE: "hardOnlyTanks",
     SHAPE: [
         [-1, -0.8],
         [-0.8, -1],
