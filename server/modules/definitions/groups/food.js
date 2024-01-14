7e1a2fb16ab6db4431191ac10b516a1cf86bd6f8
Zenphia <93544652+Zenphia@users.noreply.github.com>
2024-01-14 22:03:48 +0000

    old dreads colors ftw

--- server/modules/definitions/groups/food.js
+++ server/modules/definitions/groups/food.js
@@ -331,7 +331,7 @@ Class.hexagon = {
     VALUE: 500,
     SHAPE: 6,
     SIZE: 22,
-    COLOR: "yellow",
+    COLOR: "magenta",
     BODY: {
         DAMAGE: 1.7 * basePolygonDamage,
         DENSITY: 8,
