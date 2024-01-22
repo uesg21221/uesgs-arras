2d9227681e7cf1ca1ae4612ce39fcb8c9201023c
Zyrafaq <121723407+zyrafaq@users.noreply.github.com>
2024-01-22 16:01:03 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Check if the "ws" package is installed globally using npm
-if npm list -g ws --depth=0 | grep -q 'ws@8.16.0'; then
+if npm list ws --depth=0 | grep -q 'ws@8.16.0'; then
     echo "Dependencies have already been installed!"
 else
     echo "ws package not found. Installing dependencies."
 
     # Install the "ws" package using npm
-    npm install -g ws
+    npm install ws
 
     # Check if the installation was successful
     if [ $? -eq 0 ]; then
