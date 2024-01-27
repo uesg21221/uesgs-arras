82865717ddc7baa4f5c99c8d90b38ed7925b19dd
Taureon <45183108+Taureon@users.noreply.github.com>
2024-01-27 01:58:51 +0100

    Update entity.js

--- server/modules/live/entity.js
+++ server/modules/live/entity.js
@@ -347,25 +347,7 @@ class Gun {
             o.team = this.body.team;
             o.refreshBodyAttributes();
             o.life();
-            this.altFire ? this.master.ON(
-                undefined,
-                'altFire',
-                {
-                    gun: this,
-                    store: this.store,
-                    globalStore: this.globalStore,
-                    child: o
-                }
-            ) : this.master.ON(
-                undefined,
-                'fire',
-                {
-                    gun: this,
-                    store: this.store,
-                    globalStore: this.globalStore,
-                    child: o
-                }
-            )
+            this.master.ON(undefined, this.altFire ? 'altFire' : 'fire', { gun: this, store: this.store, globalStore: this.globalStore, child: o });
             return;
         }
 
@@ -385,27 +367,7 @@ class Gun {
         this.bulletInit(o);
         o.coreSize = o.SIZE;
 
-        this.altFire ? this.master.ON(
-            undefined, 
-            'altFire',   
-                {   
-                    gun: this, 
-                    store: this.store, 
-                    globalStore: this.
-                    globalStore, 
-                    child: o 
-                }
-            ) : this.master.ON(
-                undefined, 
-                'fire', 
-                { 
-                    gun: this, 
-                    store: this.store, 
-                    globalStore: 
-                    this.globalStore, 
-                    child: o 
-                }
-            )
+        this.master.ON(undefined, this.altFire ? 'altFire' : 'fire', { gun: this, store: this.store, globalStore: this.globalStore, child: o });
     }
     bulletInit(o) {
         // Define it by its natural properties
@@ -926,8 +888,8 @@ class Entity extends EventEmitter {
                 needsBodyAttribRefresh = true;
                 this.emit('expiredStatusEffect', entry.effect);
             }
-            if (entry.effect.tick && entry.effect.tick(this, entry.effect)) {
-                needsBodyAttribRefresh = true
+            if (entry.effect.tick && entry.effect.tick(this, entry.effect, entry.durationLeftover)) {
+                needsBodyAttribRefresh = true;
             }
         }
         this.statusEffects = lastingEffects;
@@ -1767,7 +1729,7 @@ class Entity extends EventEmitter {
             case "motor":
                 this.maxSpeed = 0;
                 if (this.topSpeed) {
-                    this.damp = a / this.topSpeed;
+                    this.damp = Math.abs(a) / this.topSpeed;
                 }
                 if (gactive) {
                     let len = Math.sqrt(g.x * g.x + g.y * g.y);
