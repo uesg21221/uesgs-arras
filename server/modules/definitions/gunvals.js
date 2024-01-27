48fa8b7b5e20dcc44ac20f921f6e9713f610d788
Taureon <45183108+Taureon@users.noreply.github.com>
2024-01-27 12:53:26 +0100

    i compensated for something

--- server/modules/definitions/gunvals.js
+++ server/modules/definitions/gunvals.js
@@ -3,7 +3,7 @@ module.exports = {
     basic: { reload: 18, recoil: 1.4, shudder: 0.1, damage: 0.75, speed: 4.5, spray: 15 },
     drone: { reload: 50, recoil: 0.25, shudder: 0.1, size: 0.6, speed: 2, spray: 0.1 },
     trap: { reload: 36, shudder: 0.25, size: 0.6, damage: 0.75, speed: 5, spray: 15, resist: 3 },
-    swarm: { reload: 45, recoil: 0.25, shudder: 0.05, size: 0.4, damage: 0.45, speed: 4, spray: 5 },
+    swarm: { reload: 36, recoil: 0.25, shudder: 0.05, size: 0.4, damage: 0.45, speed: 4, spray: 5 },
     factory: { reload: 60, shudder: 0.1, size: 0.7, damage: 0.75, speed: 3, spray: 0.1 },
     productionist: { reload: 75, recoil: 0.25, shudder: 0.05, size: 0.7, damage: 0.75, speed: 4, range: 1.5, spray: 5 },
 
