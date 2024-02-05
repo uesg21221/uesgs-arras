ba1b13438388fa65fb11187cb54fdfb6d2add86f
Taureon <45183108+Taureon@users.noreply.github.com>
2024-02-05 12:04:38 +0100

    engrish

--- server/config.js
+++ server/config.js
@@ -124,7 +124,7 @@ module.exports = {
     // Natural Spawns
 
     FOOD_CAP: 3, // Max normal food per normal tile.
-    FOOD_SPAWN_CHANCE: 0.875, // Likeliness of normal food spawn attempts succeedingg.
+    FOOD_SPAWN_CHANCE: 0.875, // Likeliness of normal food spawn attempts succeeding.
     FOOD_SPAWN_COOLDOWN: 30, // Cooldown (in game ticks) of food spawn attempts being made.
 
     FOOD_CAP_NEST: 3, // Max nest food per nest tile.
