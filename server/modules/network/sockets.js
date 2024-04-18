b39248a7ae6e0b8c6b351dc0652143f369246f13
Frostbyte178 <frostbyte178@gmail.com>
2024-04-17 18:27:04 -0700

    improved controller addition and replacement

--- server/modules/network/sockets.js
+++ server/modules/network/sockets.js
@@ -881,7 +881,6 @@ const spawn = (socket, name) => {
         util.remove(disconnections, disconnections.indexOf(recover));
         clearTimeout(recover.timeout);
         body = recover.body;
-        body.controllers = body.controllers.filter(con => !(con instanceof ioTypes.listenToPlayer));
         body.become(player);
         player.team = body.team;
     } else {
