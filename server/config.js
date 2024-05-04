8dadfa892a2d9f8fe019719e32975dc243dbbb8c
Zephiii <jte@duck.com>
2024-05-04 20:01:22 +0000

    read desc of commit
    > accidentally sets `BOSS_SPAWN_COOLDOWN` from 60 to 0
    > "fixes" it by setting `BOTS` to 60
    
    big brain moment

--- server/config.js
+++ server/config.js
@@ -111,7 +111,7 @@ module.exports = {
     LEVEL_CHEAT_CAP: 45,
 
     // Amount of player-bots to spawn.
-    BOTS: 60,
+    BOTS: 0,
 
     // How much XP player-bots get per second until they reach LEVEL_CAP.
     BOT_XP: 125,
@@ -148,7 +148,7 @@ module.exports = {
     ENEMY_SPAWN_COOLDOWN_NEST: 60, // Cooldown (in game ticks) of nest enemies spawn attempts being made.
 
     // Cooldown (in seconds) of boss spawns being announced.
-    BOSS_SPAWN_COOLDOWN: 0,
+    BOSS_SPAWN_COOLDOWN: 60,
     // The delay (in seconds) between the boss spawns being announced and the bosses actually spawning.
     // NOTE: The spawn message (ex. "A strange trembling...") takes half as long to appear than the boss.
     BOSS_SPAWN_DURATION: 5,
