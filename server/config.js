84d4fbd2d2ad7ff06af6f00d68e7149a129f45e0
Glitch (hot-adjoining-cookie) <none>
2024-06-02 20:11:59 +0000

    📼🛩️ Updated with Glitch

--- server/config.js
+++ server/config.js
@@ -3,10 +3,10 @@ module.exports = {
 
     // Game server domain.
     // If 'localhost:NUMBER', the port must equal the port setting.
-    host: "localhost:26301",
+    host: "hot-adjoining-cookie.glitch.me",
 
     // Which port to run the web server on.
-    port: 26301,
+    port: 3000,
 
     // How often to update the list of the entities that players can see.
     // Has effects of when entities are activated.
