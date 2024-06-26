0ffea23f2327c8294e8ef9f8de7730ef1fbaf177
AE0hello <141400718+AE0hello@users.noreply.github.com>
2024-06-26 16:44:30 +0200

    Fixed bugs + ready to be merged with main.

--- public/app.js
+++ public/app.js
@@ -782,92 +782,91 @@ function drawPoly(context, centerX, centerY, radius, sides, angle = 0, borderles
         } else {
             if ("string" === typeof sides) {
                 //ideally we'd preload images when mockups are loaded but im too lazy for that atm
-                if (!drawPolyImgs[sides]) {
+                if (sides.startsWith('/') | sides.startsWith('./') | sides.startsWith('http')) {
                     drawPolyImgs[sides] = new Image();
                     drawPolyImgs[sides].src = sides;
                     drawPolyImgs[sides].isBroken = false;
-                    drawPolyImgs[sides].onerror = function () {
-                        this.isBroken = true;
-                    };
+                    drawPolyImgs[sides].onerror = function() {
+                    this.isBroken = true;
                 }
-                let img = drawPolyImgs[sides];
-                if (img.isBroken || !img.complete) { // check if img is broken and draw as path2d if so
-                    let path = new Path2D(sides);
-                    context.save();
-                    context.translate(centerX, centerY);
-                    context.scale(radius, radius);
-                    context.lineWidth /= radius;
-                    context.rotate(angle);
-                    context.lineWidth *= fill ? 1 : 0.5; // Maintain constant border width
-                    if (!borderless) context.stroke(path);
-                    if (fill) context.fill(path);
-                    context.restore();
-                    return;
-                }
-                context.translate(centerX, centerY);
-                context.rotate(angle);
-                context.imageSmoothingEnabled = imageInterpolation;
-                context.drawImage(img, -radius, -radius, radius * 2, radius * 2);
-                context.imageSmoothingEnabled = true;
-                context.rotate(-angle);
-                context.translate(-centerX, -centerY);
-                return;
-            }
-            angle += sides % 2 ? 0 : Math.PI / sides;
-        }
-        if (!sides) {
-            // Circle
-            let fillcolor = context.fillStyle;
-            let strokecolor = context.strokeStyle;
-            context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
-            context.fillStyle = strokecolor;
-            context.lineWidth *= fill ? 1 : 0.5; // Maintain constant border width
-            if (!borderless) context.stroke();
-            context.closePath();
-            context.beginPath();
-            context.fillStyle = fillcolor;
-            context.arc(centerX, centerY, radius * fill, 0, 2 * Math.PI);
-            if (fill) context.fill();
-            context.closePath();
-            return;
-        } else if (sides < 0) {
-            // Star
-            if (settings.graphical.pointy) context.lineJoin = "miter";
-            sides = -sides;
-            angle += (sides % 1) * Math.PI * 2;
-            sides = Math.floor(sides);
-            let dip = 1 - 6 / (sides ** 2);
-            context.moveTo(centerX + radius * Math.cos(angle), centerY + radius * Math.sin(angle));
-            context.lineWidth *= fill ? 1 : 0.5; // Maintain constant border width
-            for (let i = 0; i < sides; i++) {
-                let htheta = ((i + 0.5) / sides) * 2 * Math.PI + angle,
-                    theta = ((i + 1) / sides) * 2 * Math.PI + angle,
-                    cx = centerX + radius * dip * Math.cos(htheta),
-                    cy = centerY + radius * dip * Math.sin(htheta),
-                    px = centerX + radius * Math.cos(theta),
-                    py = centerY + radius * Math.sin(theta);
-                /*if (curvyTraps) {
-                    context.quadraticCurveTo(cx, cy, px, py);
-                } else {
-                    context.lineTo(cx, cy);
-                    context.lineTo(px, py);
-                }*/
-                context.quadraticCurveTo(cx, cy, px, py);
-            }
-        } else if (sides > 0) {
-            // Polygon
-            angle += (sides % 1) * Math.PI * 2;
-            sides = Math.floor(sides);
-            context.lineWidth *= fill ? 1 : 0.5; // Maintain constant border width
-            for (let i = 0; i < sides; i++) {
-                let theta = (i / sides) * 2 * Math.PI + angle;
-                context.lineTo(centerX + radius * Math.cos(theta), centerY + radius * Math.sin(theta));
-            }
-        }
-        context.closePath();
-        if (!borderless) context.stroke();
-        if (fill) context.fill();
-        context.lineJoin = "round";
+  
+              let img = drawPolyImgs[sides];
+              context.translate(centerX, centerY);
+              context.rotate(angle);
+              context.imageSmoothingEnabled = imageInterpolation;
+              context.drawImage(img, -radius, -radius, radius*2, radius*2);
+              context.imageSmoothingEnabled = true;
+              context.rotate(-angle);
+              context.translate(-centerX, -centerY);
+              return;
+          }
+          let path = new Path2D(sides);
+          context.save();
+          context.translate(centerX, centerY);
+          context.scale(radius, radius);
+          context.lineWidth /= radius;
+          context.rotate(angle);
+          context.lineWidth *= fill ? 1 : 0.5; // Maintain constant border width
+          if (!borderless) context.stroke(path);
+          if (fill) context.fill(path);
+          context.restore();
+          return;
+      }
+      angle += sides % 2 ? 0 : Math.PI / sides;
+    }
+    if (!sides) {
+      // Circle
+      let fillcolor = context.fillStyle;
+      let strokecolor = context.strokeStyle;
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+      context.fillStyle = strokecolor;
+      context.lineWidth *= fill ? 1 : 0.5; // Maintain constant border width
+      if (!borderless) context.stroke();
+      context.closePath();
+      context.beginPath();
+      context.fillStyle = fillcolor;
+      context.arc(centerX, centerY, radius * fill, 0, 2 * Math.PI);
+      if (fill) context.fill();
+      context.closePath();
+      return;
+    } else if (sides < 0) {
+      // Star
+      if (settings.graphical.pointy) context.lineJoin = "miter";
+      sides = -sides;
+      angle += (sides % 1) * Math.PI * 2;
+      sides = Math.floor(sides);
+      let dip = 1 - 6 / (sides ** 2);
+      context.moveTo(centerX + radius * Math.cos(angle), centerY + radius * Math.sin(angle));
+      context.lineWidth *= fill ? 1 : 0.5; // Maintain constant border width
+      for (let i = 0; i < sides; i++) {
+          let htheta = ((i + 0.5) / sides) * 2 * Math.PI + angle,
+              theta = ((i + 1) / sides) * 2 * Math.PI + angle,
+              cx = centerX + radius * dip * Math.cos(htheta),
+              cy = centerY + radius * dip * Math.sin(htheta),
+              px = centerX + radius * Math.cos(theta),
+              py = centerY + radius * Math.sin(theta);
+          /*if (curvyTraps) {
+              context.quadraticCurveTo(cx, cy, px, py);
+          } else {
+              context.lineTo(cx, cy);
+              context.lineTo(px, py);
+          }*/
+          context.quadraticCurveTo(cx, cy, px, py);
+      }
+    } else if (sides > 0) {
+      // Polygon
+      angle += (sides % 1) * Math.PI * 2;
+      sides = Math.floor(sides);
+      context.lineWidth *= fill ? 1 : 0.5; // Maintain constant border width
+      for (let i = 0; i < sides; i++) {
+          let theta = (i / sides) * 2 * Math.PI + angle;
+          context.lineTo(centerX + radius * Math.cos(theta), centerY + radius * Math.sin(theta));
+      }
+  }
+  context.closePath();
+  if (!borderless) context.stroke();
+  if (fill) context.fill();
+  context.lineJoin = "round";
     } catch (e) { // this actually prevents to panic the client. so we will just call "resizeEvent()".
         resizeEvent();
         console.error("Uh oh, 'CanvasRenderingContext2D' has gotton an error! Error: " + e);
