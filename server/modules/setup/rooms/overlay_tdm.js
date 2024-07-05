fc5ba1a88996fd26a0195a88b269e8706f14b64e
Frostbyte178 <frostbyte178@gmail.com>
2024-07-05 11:57:39 -0700

    2tdm special map support

--- server/modules/setup/rooms/overlay_tdm.js
+++ server/modules/setup/rooms/overlay_tdm.js
@@ -57,10 +57,23 @@ locations = [
 	]
 ];
 
-for (let i = 1; i <= teams; i++) {
-	let [ spawns, protectors ] = locations[i - 1];
-	for (let [y, x] of spawns) room[y][x] = bases[`base${i}`];
-	for (let [y, x] of protectors) room[y][x] = bases[`base${i}protected`];
+if (teams === 2) {
+	let baseprotGap = Math.ceil((Config.roomHeight - 1) / 6);
+	for (let y = 0; y < Config.roomHeight; y++) {
+		room[y][0] = bases.base1;
+		room[y][Config.roomWidth - 1] = bases.base2;
+	}
+	for (let i = -2; i <= 2; i++) {
+		let y = Math.floor(Config.roomHeight / 2 - baseprotGap * i);
+		room[y][0] = bases.base1protected;
+		room[y][Config.roomWidth - 1] = bases.base2protected;
+	}
+} else {
+	for (let i = 1; i <= teams; i++) {
+		let [ spawns, protectors ] = locations[i - 1];
+		for (let [y, x] of spawns) room[y][x] = bases[`base${i}`];
+		for (let [y, x] of protectors) room[y][x] = bases[`base${i}protected`];
+	}
 }
 
 module.exports = room;
