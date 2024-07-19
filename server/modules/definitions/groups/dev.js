58a00e490dfa7df64afd37b285cc14506c1264db
Zephiii <jte@duck.com>
2024-07-19 15:05:42 +0000

    merged the remaining daily tanks

--- server/modules/definitions/groups/dev.js
+++ server/modules/definitions/groups/dev.js
@@ -1067,6 +1067,7 @@ Class.testing = menu("Testing")
 Class.addons = menu("Addon Entities")
 Class.addons.UPGRADES_TIER_0 = []
 
+// misc tanks
 Class.volute = {
     PARENT: "genericTank",
     LABEL: "Volute",
@@ -1839,6 +1840,383 @@ Class.vortex.GUNS.push(
     }
 )
 
+// misc tanks, cont.
+Class.masterBullet = {
+    PARENT: "missile",
+    FACING_TYPE: "veryfastspin",
+    MOTION_TYPE: "motor",
+    HAS_NO_RECOIL: false,
+    DIE_AT_RANGE: false,
+    GUNS: [
+        {
+            POSITION: [18, 8, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.triAngleFront]),
+                TYPE: "bullet",
+                LABEL: "Front",
+                AUTOFIRE: true,
+            },
+        },
+        {
+            POSITION: [13, 8, 1, 0, -1, 140, 0.6],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "Thruster",
+                AUTOFIRE: true,
+            },
+        },
+        {
+            POSITION: [13, 8, 1, 0, 1, 220, 0.6],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "Thruster",
+                AUTOFIRE: true,
+            },
+        },
+        {
+            POSITION: [16, 8, 1, 0, 0, 150, 0.1],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "Thruster",
+                AUTOFIRE: true,
+            },
+        },
+        {
+            POSITION: [16, 8, 1, 0, 0, 210, 0.1],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "Thruster",
+                AUTOFIRE: true,
+            },
+        },
+    ],
+}
+Class.master = {
+    PARENT: "genericTank",
+    LABEL: "Master",
+    BODY: {
+        HEALTH: base.HEALTH * 0.4,
+        SHIELD: base.SHIELD * 0.4,
+        DENSITY: base.DENSITY * 0.3,
+    },
+    DANGER: 8,
+    GUNS: [
+        {
+            POSITION: [18, 16, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic]),
+                TYPE: "masterBullet",
+                MAX_CHILDREN: 4,
+                DESTROY_OLDEST_CHILD: true,
+            },
+        },
+        {
+            POSITION: [13, 8, 1, 0, -1, 140, 0.6],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "thruster",
+            },
+        },
+        {
+            POSITION: [13, 8, 1, 0, 1, 220, 0.6],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "thruster",
+            },
+        },
+        {
+            POSITION: [16, 8, 1, 0, 0, 150, 0.1],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "thruster",
+            },
+        },
+        {
+            POSITION: [16, 8, 1, 0, 0, 210, 0.1],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "thruster",
+            },
+        },
+    ],
+}
+Class.jumpSmasher = {
+    PARENT: "genericSmasher",
+    LABEL: "Jump Smasher",
+    DANGER: 7,
+    UPGRADE_TOOLTIP: "[DEV NOTE] This tank does not function as intended yet!",
+    TURRETS: [
+        {
+            POSITION: [21.5, 0, 0, 0, 360, 0],
+            TYPE: "smasherBody"
+        }
+    ]
+}
+Class.lamgSpinnerTurret = {
+    PARENT: "genericTank",
+    LABEL: "Spinner Turret",
+    COLOR: "grey",
+    GUNS: weaponArray({
+        POSITION: [15, 3.5, 1, 0, 0, 0, 0]
+    }, 10)
+}
+Class.literallyAMachineGun = {
+    PARENT: "genericTank",
+    LABEL: "Literally a Machine Gun",
+    DANGER: 7,
+    BODY: {
+        FOV: base.FOV * 1.2
+    },
+    UPGRADE_TOOLTIP: "[DEV NOTE] This tank does not function as intended yet!",
+    TURRETS: [
+        {
+            POSITION: [10, 14, 0, 0, 0, 1],
+            TYPE: "lamgSpinnerTurret"
+        }
+    ],
+    GUNS: [
+        {
+            POSITION: [22, 8, 1, 0, 0, 0, 0]
+        }
+    ]
+}
+
+// literally a tank
+class io_turretWithMotion extends IO {
+    constructor(b, opts = {}) {
+        super(b)
+    }
+    think(input) {
+        return {
+            target: this.body.master.velocity,
+            main: true,
+        };
+    }
+}
+ioTypes.turretWithMotion = io_turretWithMotion
+Class.latTop = makeDeco(0)
+Class.latDeco1 = {
+    PARENT: "genericTank",
+    LABEL: "Tank Deco",
+    FACING_TYPE: ["turnWithSpeed"],
+    COLOR: "#5C533F",
+    SHAPE: "M -1 -2 C -1 -2 -1 -3 0 -3 C 1 -3 1 -2 1 -2 V 2 C 1 2 1 3 0 3 C -1 3 -1 2 -1 2 V -2",
+    MIRROR_MASTER_ANGLE: true,
+}
+Class.latDeco2 = {
+    PARENT: "genericTank",
+    LABEL: "Tank Deco",
+    FACING_TYPE: ["turnWithSpeed"],
+    COLOR: "#5C533F",
+    SHAPE: "M -2 0 H 2 L 0 1 L -2 0",
+    MIRROR_MASTER_ANGLE: true,
+}
+Class.latDeco3 = {
+    PARENT: "genericTank",
+    LABEL: "Tank Deco",
+    FACING_TYPE: ["turnWithSpeed"],
+    COLOR: "#3F3B2D",
+    SHAPE: "M -10 -1 L 10 -1 L 10 1 L -10 1 L -10 -1",
+    MIRROR_MASTER_ANGLE: true,
+}
+Class.latRight = {
+    PARENT: "genericTank",
+    LABEL: "Tank Side",
+    FACING_TYPE: ["turnWithSpeed"],
+    COLOR: "#96794E",
+    SHAPE: "M -6 0 H 5 V 1 C 5 2 4 2 4 2 H -5 C -6 2 -6 1 -6 1 V 0",
+    MIRROR_MASTER_ANGLE: true,
+    TURRETS: [
+        {
+            POSITION: [4.8, 31, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, 24, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, 17, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, -42, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, -35, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, -28, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [18, -5, 0, 0, 0, 1],
+            TYPE: "latDeco2",
+        },
+    ]
+}
+Class.latLeft = {
+    PARENT: "genericTank",
+    LABEL: "Tank Side",
+    FACING_TYPE: ["turnWithSpeed"],
+    COLOR: "#96794E",
+    SHAPE: "M -5 0 H 6 V 1 C 6 2 5 2 5 2 H -4 C -5 2 -5 1 -5 1 V 0",
+    MIRROR_MASTER_ANGLE: true,
+    TURRETS: [
+        {
+            POSITION: [4.8, -31, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, -24, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, -17, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, 42, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, 35, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [4.8, 28, 10, 0, 0, 1],
+            TYPE: "latDeco1",
+        },
+        {
+            POSITION: [18, 5, 0, 0, 0, 1],
+            TYPE: "latDeco2",
+        },
+    ]
+}
+Class.latBase = {
+    PARENT: "genericTank",
+    LABEL: "Tank Base",
+    CONTROLLERS: ["turretWithMotion"],
+    COLOR: "#96794E",
+    SHAPE: [
+        [1.1, 1],
+        [1.4, 0],
+        [1.1, -1],
+        [-1.1, -1],
+        [-0.8, 0],
+        [-1.1, 1]
+    ],
+    GUNS: [
+        {
+            POSITION: [16, 5.5, 1, 1, 6.5, 0, 0]
+        },
+        {
+            POSITION: [14.5, 5.5, 1, 1, 6.5, 0, 0]
+        },
+        {
+            POSITION: [13, 5.5, 1, 1, 6.5, 0, 0]
+        },
+        {
+            POSITION: [16, 5.5, 1, 1, -6.5, 0, 0]
+        },
+        {
+            POSITION: [14.5, 5.5, 1, 1, -6.5, 0, 0]
+        },
+        {
+            POSITION: [13, 5.5, 1, 1, -6.5, 0, 0]
+        },
+        {
+            POSITION: [13, 5.5, 1, 1, 6.5, 180, 0]
+        },
+        {
+            POSITION: [11.5, 5.5, 1, 1, 6.5, 180, 0]
+        },
+        {
+            POSITION: [10, 5.5, 1, 1, 6.5, 180, 0]
+        },
+        {
+            POSITION: [8.5, 5.5, 1, 1, 6.5, 180, 0]
+        },
+        {
+            POSITION: [13, 5.5, 1, 1, -6.5, 180, 0]
+        },
+        {
+            POSITION: [11.5, 5.5, 1, 1, -6.5, 180, 0]
+        },
+        {
+            POSITION: [10, 5.5, 1, 1, -6.5, 180, 0]
+        },
+        {
+            POSITION: [8.5, 5.5, 1, 1, -6.5, 180, 0]
+        },
+    ],
+    TURRETS: [
+        {
+            POSITION: [5.3, 0, -10, 0, 0, 1],
+            TYPE: "latLeft",
+        },
+        {
+            POSITION: [5.3, 0, -10, 180, 0, 1],
+            TYPE: "latRight",
+        },
+        {
+            POSITION: [2, 0, -1.4, 90, 0, 1],
+            TYPE: "latDeco3",
+        },
+    ]
+}
+Class.literallyATank = {
+    PARENT: "genericTank",
+    DANGER: 6,
+    BODY: {
+        HEALTH: base.HEALTH * 1.2,
+    },
+    LABEL: "Literally a Tank",
+    SHAPE: "M -1 -1 H 0 C 1 -1 1 0 1 0 C 1 0 1 1 0 1 H -1 V -1",
+    GUNS: [
+        {
+            POSITION: [30, 8, 1, 0, 0, 0, 0]
+        },
+        {
+            POSITION: [4, 8, -1.4, 8, 0, 0, 0]
+        },
+        {
+            POSITION: [12, 8, 1.3, 30, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, { reload: 3, damage: 1.2, shudder: 0.5 }]),
+                TYPE: "developerBullet"
+            }
+        },
+        {
+            POSITION: [2, 11, 1, 34, 0, 0, 0]
+        }
+    ],
+    TURRETS: [
+        {
+            POSITION: [15, 0, 0, 0, 360, 1],
+            TYPE: [ "latTop", { COLOR: "#5C533F" } ],
+        },
+        {
+            POSITION: [10, 0, 0, 0, 360, 1],
+            TYPE: [ "latTop", { COLOR: "#736245" } ],
+        },
+        {
+            POSITION: [35, 0, 0, 0, 360, 0],
+            TYPE: [ "latBase", { COLOR: "#96794E" } ],
+        },
+    ]
+}
+
 let testLayeredBoss = new LayeredBoss("testLayeredBoss", "Test Layered Boss", "terrestrial", 7, 3, "terrestrialTrapTurret", 5, 7, {SPEED: 10});
 testLayeredBoss.addLayer({gun: {
     POSITION: [3.6, 7, -1.4, 8, 0, null, 0],
@@ -2238,7 +2616,7 @@ Class.flangle = {
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
                 TYPE: "bullet",
-                LABEL: "Thruster",
+                LABEL: "thruster",
             },
         },
         {
@@ -2246,7 +2624,7 @@ Class.flangle = {
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
                 TYPE: "bullet",
-                LABEL: "Thruster",
+                LABEL: "thruster",
             },
         },
     ],
@@ -2269,7 +2647,7 @@ Class.flooster = {
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
                 TYPE: "bullet",
-                LABEL: "Thruster",
+                LABEL: "thruster",
             },
         },
         {
@@ -2277,7 +2655,7 @@ Class.flooster = {
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
                 TYPE: "bullet",
-                LABEL: "Thruster",
+                LABEL: "thruster",
             },
         },
         {
@@ -2285,7 +2663,7 @@ Class.flooster = {
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
                 TYPE: "bullet",
-                LABEL: "Thruster",
+                LABEL: "thruster",
             },
         },
         {
@@ -2293,7 +2671,7 @@ Class.flooster = {
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
                 TYPE: "bullet",
-                LABEL: "Thruster",
+                LABEL: "thruster",
             },
         },
     ],
@@ -2316,7 +2694,7 @@ Class.flace = {
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
                 TYPE: "bullet",
-                LABEL: "Thruster",
+                LABEL: "thruster",
             },
         },
         {
@@ -2324,7 +2702,7 @@ Class.flace = {
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
                 TYPE: "bullet",
-                LABEL: "Thruster",
+                LABEL: "thruster",
             },
         },
     ],
@@ -2339,7 +2717,7 @@ Class.flace = {
 
 Class.developer.UPGRADES_TIER_0 = ["tanks", "bosses", "spectator", "levels", "teams", "eggGenerator", "testing", "addons"]
     Class.tanks.UPGRADES_TIER_0 = ["basic", "unavailable", "arenaCloser", "dominators", "sanctuaries", "mothership", "baseProtector", "antiTankMachineGun"]
-        Class.unavailable.UPGRADES_TIER_0 = ["flail", "healer", "volute", "whirlwind"]
+        Class.unavailable.UPGRADES_TIER_0 = ["flail", "jumpSmasher", "healer", "literallyAMachineGun", "literallyATank", "master", "volute", "whirlwind"]
             Class.flail.UPGRADES_TIER_2 = ["doubleFlail", "mace", "flangle"]
                 Class.doubleFlail.UPGRADES_TIER_3 = ["tripleFlail"]
                 Class.mace.UPGRADES_TIER_3 = ["bigMama", "itHurtsDontTouchIt", "flace"]
