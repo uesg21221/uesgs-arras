72463fa303b4fd3f857ae573fcc6dcaa66923d12
zephiii <jte@duck.com>
2024-10-27 14:16:36 +0000

    folder

---
+++ server/modules/definitions/addons/arras/arras.js
@@ -0,0 +1,4 @@
+const { combineStats, makeAuto, makeOver, makeDeco, makeGuard, makeBird, makeRadialAuto, weaponArray } = require('../../facilitators.js');
+const { base, statnames, dfltskl, smshskl } = require('../../constants.js');
+require('../../groups/generics.js');
+const g = require('../../gunvals.js');
